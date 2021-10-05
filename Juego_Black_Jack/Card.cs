1daa03688e1d21d2d7be14f93441774ae5efe3cf
Juan Sebastian Pérez Millares <89596142+JUANPER-GITHUB-2021@users.noreply.github.com>
2021-10-04 22:15:34 -0500

    Agregar archivos de proyecto.

---
+++ Juego_Black_Jack/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_Black_Jack
+{
+    public enum Suit
+    {
+        Clubs,
+        Spades,
+        Diamonds,
+        Hearts
+    }
+    public enum Face
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+    public class Card
+    {
+
+    }
+}
---
+++ Juego_Black_Jack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_Black_Jack
+{
+    public class Program
+    {
+        private static Deck deck = new Deck();
+        private static Player player = new Player();
+        private enum RoundResult
+        {
+            PUSH,
+            PLAYER_WIN,
+            PLAYER_BUST,
+            PLAYER_BLACKJACK,
+            DEALER_WIN,
+            SURRENDER,
+            INVALID_BET
+        }
+        static void InitializeHands()
+        {
+            deck.Initialize();
+
+            player.Hand = deck.DealHand();
+            Dealer.HiddenCards = deck.DealHand();
+            Dealer.RevealedCards = new List<Card>();
+
+            // If hand contains two aces, make one Hard.
+            if (player.Hand[0].Face == Face.Ace && player.Hand[1].Face == Face.Ace)
+            {
+                player.Hand[1].Value = 1;
+            }
+
+            if (Dealer.HiddenCards[0].Face == Face.Ace && Dealer.HiddenCards[1].Face == Face.Ace)
+            {
+                Dealer.HiddenCards[1].Value = 1;
+            }
+
+            Dealer.RevealCard();
+
+            player.WriteHand();
+            Dealer.WriteHand();
+        }
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
